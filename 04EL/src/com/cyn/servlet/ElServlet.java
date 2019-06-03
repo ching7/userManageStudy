3f1243cf29bc321ef2ec2646f493beed6bef6d72
unknown <775608598@qq.com>
2019-06-03 23:01:43 +0800

    同步工程

---
+++ 04EL/src/com/cyn/servlet/ElServlet.java
@@ -0,0 +1,81 @@
+package com.cyn.servlet;
+
+import com.cyn.bean.Address;
+import com.cyn.bean.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 问题：
+ *  servlet 处理请求后，使用作用域对象将数据返回给对应的jsp文件
+ *  jsp中怎么获取数据
+ *
+ *  使用：
+ *      传统方式：使用java脚本段语句在jsp页面中
+ *
+ */
+@WebServlet("/elServlet")
+public class ElServlet extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //设置请求编码格式
+        req.setCharacterEncoding("utf-8");
+        //设置响应编码格式
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        //获取请求信息
+        String uname = req.getParameter("uname");
+        String pwd=req.getParameter("pwd");
+        //处理请求信息
+        System.out.println(uname +":" + pwd);
+        //返回处理数据
+            //使用request作用域流转数据
+            // 1 普通字符串
+            req.setAttribute("str","晚上适合学习！！");
+            // 2 对象
+            User user = new User();
+            user.setUname("李武");
+            user.setUid(1);
+            user.setAddr(new Address("浙江","杭州","滨江"));
+            user.setFav("电影");
+            req.setAttribute("user",user);
+            // 3 List集合
+            // 1) string 集合
+            List<String> list = new ArrayList<String>();
+            list.add("王二麻子");
+            list.add("李狗蛋");
+            list.add("王春花");
+            req.setAttribute("list",list);
+            // 2) 对象集合
+            User user1 = new User();
+            user1.setUname("与大爷");
+            User user2 = new User();
+            user2.setUname("桃儿");
+            List<User> userList = new ArrayList<User>();
+            userList.add(user1);
+            userList.add(user2);
+            req.setAttribute("userList",userList);
+            //4 Map 集合
+                //普通字符串
+                Map<String,String> strMap = new HashMap<String,String>();
+                strMap.put("杭州","996");
+                req.setAttribute("strMap",strMap);
+                //对象
+                Map<String,User> strMap1 = new HashMap<String,User>();
+                strMap1.put("daYe",user1);
+                req.setAttribute("userMap",strMap1);
+
+        //直接响应
+            //请求转发
+            req.getRequestDispatcher("/el.jsp").forward(req,resp);
+            //重定向
+    }
+}
---
+++ 04EL/web/el.jsp
@@ -0,0 +1,87 @@
+<%@ page import="com.cyn.bean.User" %>
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="java.util.HashMap" %><%--
+  Created by IntelliJ IDEA.
+  User: 77560
+  Date: 2019-06-01
+  Time: 21:32
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--
+<!--  使用传统方式，获取作用域对象数据 -->
+<h3>EL表达式学习：传统方式获取数据</h3>
+<b><%=request.getParameter("uname")%></b>
+<hr>
+<b><%=request.getAttribute("str")%></b>
+<hr>
+<b><%=((User)request.getAttribute("user")).toString()%>></b>
+<hr>
+<b><%=((ArrayList)request.getAttribute("list")).get(0)%></b>
+<hr>
+<b><%=((User)(((ArrayList)request.getAttribute("userList")).get(0))).getUname()%></b>
+<hr>
+<b><%=((HashMap)request.getAttribute("strMap")).get("杭州")%></b>
+<hr>
+<b><%=((User)((HashMap)request.getAttribute("userMap")).get("daYe")).getUname()%></b>
+
+--%>
+
+<%--
+    EL表达式
+
+    作用：获取作用域内的数据
+
+    注意：获取的是pageContext,request,session,application 四个对象中的数据，其他以改不理会
+    找到了则获取返回，找不到则什么都不做
+
+    语法：${表达式}
+        表达式：
+            request对象：存储请求数据，----> param.键名 （一键一值）
+            request对象：存储请求数据，----> paramValues.键名 （一键多值），返回数组
+
+            setAttribute设置的数据
+                ${键名} ----> 字符串直接返回
+                ${键名} ----> 普通对象${键名.属性名}
+                ${键名} ----> list集合${键名[角标]}
+                ${键名} ----> map集合${键名.map集合储存的键名}
+
+    作用域查找顺序：
+
+    默认查找顺序：
+    EL按作用域的大小去依次查找数据
+    （注意作用域的持续时间）
+
+    指定查找顺序：
+--%>
+<h3>EL表达式学习：EL表达式（获取作用域的数据）</h3>
+<b>${param.uname}</b>
+<b>${paramValues.uname[1]}</b>
+<hr>
+<b>${str}</b>
+<hr>
+<b>${user}</b>
+<hr>
+<b>${list[0]}</b>
+<hr>
+<b>${userList[0].uname}</b>
+<hr>
+<b>${strMap.杭州}</b>
+<hr>
+<b>${userMap.daYe.uname}</b>
+
+<h3>EL表达式学习：EL作用查找顺序</h3>
+<%
+    <%--
+    EL按作用域的大小去依次查找数据
+    （注意作用域的持续时间）
+    --%>
+    pageContext.setAttribute("hello","pageContext");
+    request.setAttribute("hello","request");
+    session.setAttribute("hello","session");
+    application.setAttribute("hello","application");
+
+%>
+${hello}
+${requestScope.hello}
